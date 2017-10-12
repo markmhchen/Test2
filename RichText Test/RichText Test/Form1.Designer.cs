4a0c9ce17b9a07bce1ccd50bd96cda1c5c6f5af4
= <=>
2017-10-12 14:27:47 -0700

    First commit here

---
+++ RichText Test/RichText Test/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RichText_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Salmon;
+            this.richTextBox1.Location = new System.Drawing.Point(25, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(217, 294);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 110);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Line ";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(344, 182);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(248, 297);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(263, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 347);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RichText Test/RichText Test/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RichText_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        bool color = false;
+        int count = 0;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //richTextBox1.Text = textBox1.Text + "\r\n" + richTextBox1.Text;
+            if (color)
+            {
+                richTextBox1.SelectionColor = Color.GreenYellow;
+                color = !color;
+            }
+            else
+            {
+                richTextBox1.SelectionColor = Color.Salmon;
+                color = !color;
+
+            }
+            string str = textBox1.Text;
+            char[] newStr = new char[str.Length];
+            byte b, b0, b1;
+            for (int i = 0; i < str.Length; i++)                
+            {
+                if (i > 0) b0 = (byte)str[i-1];
+                if (i < str.Length - 1) b1 = (byte)str[i+1];
+                
+                b = (byte)str[i];
+                if (b == 10 || b == 13)
+                    newStr[i] = ' ';
+                else
+                    newStr[i] = str[i];
+            }
+
+            str = new string(newStr);
+
+            int len = textBox1.Text.Length;
+           // richTextBox1.SelectedText = Environment.NewLine + textBox1.Text + count.ToString();
+            richTextBox1.SelectedText = textBox1.Text + count.ToString() + "\r\n";
+            count++;
+           // WriteTextToRichTextBox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Font font = new Font("Tahoma", 8, FontStyle.Regular);
+            richTextBox1.SelectionFont = font;
+
+        }
+
+        private void WriteTextToRichTextBox()
+        {
+            // Clear all text from the RichTextBox;
+            richTextBox1.Clear();
+            // Set the font for the opening text to a larger Arial font;
+            richTextBox1.SelectionFont = new Font("Arial", 16);
+            // Assign the introduction text to the RichTextBox control.
+            richTextBox1.SelectedText = "The following is a list of bulleted items:" + "\n";
+            richTextBox1.AppendText("Appedned Text 1");
+            // Set the Font for the first item to a smaller size Arial font.
+            richTextBox1.SelectionFont = new Font("Arial", 12);
+            // Specify that the following items are to be added to a bulleted list.
+            richTextBox1.SelectionBullet = true;
+            // Set the color of the item text.
+            richTextBox1.SelectionColor = Color.Red;
+            // Assign the text to the bulleted item.
+            richTextBox1.SelectedText = "Apples" + "\n";
+            richTextBox1.AppendText("Appedned Text 2");
+
+            // Apply same font since font settings do not carry to next line.
+            richTextBox1.SelectionFont = new Font("Arial", 12);
+            richTextBox1.SelectionColor = Color.Orange;
+            richTextBox1.SelectedText = "Oranges" + "\n";
+            richTextBox1.SelectionFont = new Font("Arial", 12);
+            richTextBox1.SelectionColor = Color.Purple;
+            richTextBox1.SelectedText = "Grapes" + "\n";
+            // End the bulleted list.
+            richTextBox1.SelectionBullet = false;
+            // Specify the font size and string for text displayed below bulleted list.
+            richTextBox1.SelectionFont = new Font("Arial", 16);
+            richTextBox1.SelectedText = "Bulleted Text Complete!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+            #region --File open testing
+            ////string fileName;
+
+            ////fileName = Application.StartupPath + "\\a\\b\\c";
+            ////if (!Directory.Exists(fileName)) Directory.CreateDirectory(fileName);
+            ////fileName += "\\file2.txt";
+            ////using (StreamWriter sw = new StreamWriter(fileName, true))
+            ////{
+            ////    sw.WriteLine("Hello at " + DateTime.Now.ToString() + "\r\n");
+            ////}
+            #endregion
+        }
+    }
+}
